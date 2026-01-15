98bdf0847a2deabef9d284bf7630052d611036bf
sowmyasri-d9 <95292444+sowmyasri-d9@users.noreply.github.com>
2026-01-15 13:31:24 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,17 +2,12 @@ const mongoose = require("mongoose");
 const DEFAULT_ADMIN = require("../controllers/authController")
 
 const connectDB = async () => {
-  // const LIVE_URL = "mongodb+srv://slemach:Slemach-321@cluster0.pnqsz.mongodb.net/?appName=Cluster0"
   const LIVE_URL = "mongodb+srv://sowmya:Sowmya%401234@loginschema.6wuwv.mongodb.net/LogInSchema"
-  // const LIVE_URL = "mongodb+srv://sowmya:Sowmya%401234@loginschema.6wuwv.mongodb.net/?retryWrites=true&w=majority&appName=LogInSchema"
 
   try {
-    await mongoose.connect(LIVE_URL); // Removed deprecated options
-    console.log("✅ MongoDB Connected");
-    console.log("Updated URI");
+    await mongoose.connect(LIVE_URL);
     await DEFAULT_ADMIN.createAdminUser();
   } catch (err) {
-    console.error("❌ MongoDB Connection Error:", err.message);
     process.exit(1);
   }
 };
